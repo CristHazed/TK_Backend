48ac32fb940609bf9a4a983df0d6c4bd0cd4bd83
CristHazed <cristianlino1123@gmail.com>
2026-08-25 05:53:09 +0800

    Refactor server.js to export app instead of listening
    Removed server listening logic and exported app. - Server less hosting update

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,4 @@ app.use(express.static('public'));
 app.use('/api/', registerRouter);
 app.use('/api/' , loginRouter);
 
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
